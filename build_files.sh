ff7f2f4f1c03bf368ee17bed3eef1209ee07a74a
ubuntucosmas <cosmasasango12@gmail.com>
2024-09-09 23:33:38 +0300

    initial

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
+# Install Python if not already installed
+apt-get update
+apt-get install -y python3 python3-pip
+
+# Install Python dependencies
+pip3 install -r requirements.txt
+
 pip freeze > requirements.txt
 python manage.py collectstatic
