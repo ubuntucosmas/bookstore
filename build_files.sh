8f2d8639d347ef12a70b3a537b23303d9e5e96f8
ubuntucosmas <cosmasasango12@gmail.com>
2024-09-09 23:55:08 +0300

    initial

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,4 @@
-
+python manage.py collectstatic
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
-
-
 $ pip install -r requirements.txt
-python manage.py collectstatic
